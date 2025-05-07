7f3b8495a77ac6ed214ac73f40536023de962fda
kaleseed <kaleseed@g.com>
2025-05-06 23:04:20 -0400

    move CNAME file to right folder;

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,6 @@ git pull
 mkdocs gh-deploy --clean
 
 #cat docs.welsim.com > CNAME
-git add CNAME
-git commit -a -m "add CNAME file"
-git push origin gh-pages
+#git add CNAME
+#git commit -a -m "add CNAME file"
+#git push origin gh-pages
